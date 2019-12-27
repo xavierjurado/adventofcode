3a9053bfde8fbe6156d40c12970bf5c051d061e2
Xavier Jurado <xavier.jurado@gmail.com>
2019-12-27 15:48:01 +0100

    Day 9 part 2 test

--- Puzzles/PuzzlesTests/09-sensor-boost-test.swift
+++ Puzzles/PuzzlesTests/09-sensor-boost-test.swift
@@ -11,8 +11,17 @@ class SensorBoostTest: XCTestCase {
         sut.input = TestInput(values: [1])
         sut.output = outputSpy
         XCTAssertNoThrow(try sut.execute())
-        XCTAssertEqual(outputSpy.values.count, 1)
+        XCTAssertEqual(outputSpy.values, [3460311188])
+    }
 
-        print(outputSpy.values[0])
+    func testPartTwo() {
+        let scanner = SingleValueScanner<Int>(testCaseName: "09", separator: CharacterSet(charactersIn: ","))
+        let program = scanner.parse()
+        let sut = Intcode(memory: program)
+        let outputSpy = TestOutput()
+        sut.input = TestInput(values: [2])
+        sut.output = outputSpy
+        XCTAssertNoThrow(try sut.execute())
+        XCTAssertEqual(outputSpy.values, [42202])
     }
 }
