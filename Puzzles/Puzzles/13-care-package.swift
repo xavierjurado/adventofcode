58cc0efa0dcb43680c93df8aa4e2cf5b80d5971a
Xavier Jurado <xavier.jurado@gmail.com>
2020-01-12 17:52:12 +0100

    Day 13 Part 2

--- Puzzles/Puzzles/13-care-package.swift
+++ Puzzles/Puzzles/13-care-package.swift
@@ -1,10 +1,14 @@
 import Foundation
 
-class ArcadeScreenInterface: OutputBuffer {
+class ArcadeInterface: OutputBuffer, InputBuffer {
 
     struct XY: Hashable {
         var x: Int
         var y: Int
+
+        static var zero: XY {
+            return XY(x: 0, y: 0)
+        }
     }
 
     enum Tile: Int {
@@ -13,12 +17,37 @@ class ArcadeScreenInterface: OutputBuffer {
         case block = 2
         case paddle = 3
         case ball = 4
+
+        func toString() -> String {
+            switch self {
+            case .empty:
+                return " "
+            case .wall:
+                return "▓"
+            case .block:
+                return "░"
+            case .paddle:
+                return "█"
+            case .ball:
+                return "o"
+            }
+        }
     }
 
+    enum JoystickPosition: Int {
+        case neutral = 0
+        case left = -1
+        case right = 1
+    }
+
+    var joystick: JoystickPosition?
+    private(set) var score: Int = 0
     private var xPosition: Int = 0
     private var yPosition: Int = 0
     private var writeCount: Int = 0
     private var grid: [XY: Tile] = [:]
+    private(set) var ballPosition: XY = .zero
+    private(set) var paddlePosition: XY = .zero
 
     func write(value: Int) {
         defer {
@@ -31,8 +60,18 @@ class ArcadeScreenInterface: OutputBuffer {
         case 1:
             yPosition = value
         case 2:
-            let tile = Tile(rawValue: value)!
-            grid[XY(x: xPosition, y: yPosition)] = tile
+            if xPosition == -1, yPosition == 0 {
+                score = value
+            } else {
+                let tile = Tile(rawValue: value)!
+                let position = XY(x: xPosition, y: yPosition)
+                grid[position] = tile
+                if tile == .paddle {
+                    paddlePosition = position
+                } else if tile == .ball {
+                    ballPosition = position
+                }
+            }
         default:
             fatalError()
         }
@@ -41,15 +80,70 @@ class ArcadeScreenInterface: OutputBuffer {
     var blockTiles: Int {
         return grid.filter { $0.value == .block }.count
     }
+
+    func read() -> Int {
+        let value = joystick!.rawValue
+        joystick = nil
+        return value
+    }
+
+    func hasData() -> Bool {
+        return joystick != nil
+    }
+
+    func printGrid() {
+        let maxX = grid.keys.max { $0.x < $1.x }.map { $0.x }!
+        let maxY = grid.keys.max { $0.y < $1.y }.map { $0.y }!
+
+        var y = 0
+        while y < maxY {
+            var line = ""
+            var x = 0
+            while x < maxX {
+                let tile = grid[XY(x: x, y: y)]!
+                line += tile.toString()
+                x += 1
+            }
+            print(line)
+            y += 1
+        }
+    }
 }
 
 class CarePackage {
 
     func solvePartOne(memory: [Int]) -> Int {
-        let screen = ArcadeScreenInterface()
+        let screen = ArcadeInterface()
         let intcode = Intcode(memory: memory)
         intcode.output = screen
         try? intcode.execute()
+        screen.printGrid()
         return screen.blockTiles
+    }
+
+    func solvePartTwo(memory: [Int], debug: Bool = false) -> Int {
+        var memory = memory
+        memory[0] = 2
+        let interface = ArcadeInterface()
+        let intcode = Intcode(memory: memory)
+        intcode.output = interface
+        intcode.input = interface
+
+        repeat {
+            try? intcode.execute()
+            if debug {
+                interface.printGrid()
+            }
+
+            if interface.paddlePosition.x < interface.ballPosition.x {
+                interface.joystick = .right
+            } else if interface.paddlePosition.x > interface.ballPosition.x {
+                interface.joystick = .left
+            } else {
+                interface.joystick = .neutral
+            }
+        } while intcode.awaitingInput
+
+        return interface.score
     }
 }
--- Puzzles/PuzzlesTests/13-care-package-test.swift
+++ Puzzles/PuzzlesTests/13-care-package-test.swift
@@ -9,4 +9,11 @@ class CarePackageTest: XCTestCase {
         let sut = CarePackage()
         XCTAssertEqual(sut.solvePartOne(memory: program), 251)
     }
+
+    func testPartTwo() {
+        let scanner = SingleValueScanner<Int>(testCaseName: "13", separator: CharacterSet(charactersIn: ","))
+        let program = scanner.parse()
+        let sut = CarePackage()
+        XCTAssertEqual(sut.solvePartTwo(memory: program), 12779)
+    }
 }
