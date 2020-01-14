b225f3b0110c66d07dba71df32252d4e19ff75f1
Xavier Jurado <xavier.jurado@gmail.com>
2020-01-15 00:14:21 +0100

    Day 14 Part 1

--- Puzzles/Puzzles/14-space-stoichiometry.swift
+++ Puzzles/Puzzles/14-space-stoichiometry.swift
@@ -5,12 +5,16 @@ class SpaceStoichiometry {
     static let ore = "ORE"
     static let fuel = "FUEL"
 
-    struct Chemical: Hashable {
+    struct Chemical: Hashable, CustomStringConvertible {
         let id: String
         let quantity: Int
 
         func times(_ factor: Int) -> Chemical {
             return Chemical(id: id, quantity: quantity * factor)
+        }
+
+        var description: String {
+            return "\(quantity) \(id)"
         }
     }
 
@@ -19,6 +23,7 @@ class SpaceStoichiometry {
         let output: Chemical
     }
 
+    // this could be done also with a simple topological sort
     func solvePartOne(reactions: [Reaction]) -> Int {
 
         var reactionsByOutput: [String: Reaction] = [:]
@@ -27,16 +32,26 @@ class SpaceStoichiometry {
         }
 
         let fuel = reactionsByOutput[SpaceStoichiometry.fuel]!
-        var materials: [String: Int] = Dictionary(grouping: fuel.input, by: { $0.id }).mapValues { $0.count }
+        var materials: [String: Int] = Dictionary(grouping: fuel.input, by: { $0.id }).mapValues { $0.reduce(0, { $0 + $1.quantity}) }
+        var leftovers: [String: Int] = [:]
         var ores: Int = 0
 
         while !materials.isEmpty {
             let wantedChemical = materials.first!
             let wantedId = wantedChemical.key
-            let wantedQuantity = wantedChemical.value
+            var wantedQuantity = wantedChemical.value
 
             defer {
                 materials.removeValue(forKey: wantedId)
+            }
+
+            if let leftover = leftovers[wantedId], leftover > 0 {
+                wantedQuantity -= leftover
+                wantedQuantity = max(0, wantedQuantity)
+                leftovers[wantedId] = leftover - (materials[wantedId]! - wantedQuantity)
+                if wantedQuantity == 0 {
+                    continue
+                }
             }
 
             guard wantedId != SpaceStoichiometry.ore else {
@@ -48,16 +63,17 @@ class SpaceStoichiometry {
             let neededReactionChemicals: [Chemical]
             if neededReaction.output.quantity >= wantedQuantity {
                 neededReactionChemicals = neededReaction.input
+                leftovers[wantedId, default: 0] += neededReaction.output.quantity - wantedQuantity
             } else {
                 let (q, r) = wantedQuantity.quotientAndRemainder(dividingBy: neededReaction.output.quantity)
                 let factor = q + (r > 0 ? 1 : 0)
                 neededReactionChemicals = neededReaction.input.map { $0.times(factor) }
+                leftovers[wantedId, default: 0] += neededReaction.output.quantity * factor - wantedQuantity
             }
 
             for c in neededReactionChemicals {
                 materials[c.id, default: 0] += c.quantity
             }
-
         }
 
         return ores
--- Puzzles/PuzzlesTests/14-space-stoichiometry-test.swift
+++ Puzzles/PuzzlesTests/14-space-stoichiometry-test.swift
@@ -19,4 +19,49 @@ class SpaceStoichiometryTest: XCTestCase {
 
         XCTAssertEqual(sut.solvePartOne(reactions: reactions), 31)
     }
+
+    func testSecondExample() {
+        let sut = SpaceStoichiometry()
+        let reactions = [
+            Reaction(input: [Chemical(id: "ORE", quantity: 9)], output: Chemical(id: "A", quantity: 2)),
+            Reaction(input: [Chemical(id: "ORE", quantity: 8)], output: Chemical(id: "B", quantity: 3)),
+            Reaction(input: [Chemical(id: "ORE", quantity: 7)], output: Chemical(id: "C", quantity: 5)),
+            Reaction(input: [Chemical(id: "A", quantity: 3), Chemical(id: "B", quantity: 4)], output: Chemical(id: "AB", quantity: 1)),
+            Reaction(input: [Chemical(id: "B", quantity: 5), Chemical(id: "C", quantity: 7)], output: Chemical(id: "BC", quantity: 1)),
+            Reaction(input: [Chemical(id: "C", quantity: 4), Chemical(id: "A", quantity: 1)], output: Chemical(id: "CA", quantity: 1)),
+            Reaction(input: [Chemical(id: "AB", quantity: 2), Chemical(id: "BC", quantity: 3),  Chemical(id: "CA", quantity: 4)], output: Chemical(id: "FUEL", quantity: 1)),
+        ]
+
+        XCTAssertEqual(sut.solvePartOne(reactions: reactions), 165)
+    }
+
+    func testPartOne() {
+        let characterSet = CharacterSet(charactersIn: " ,=>")
+        let scanner = SingleValueScanner<Reaction>(testCaseName: "14", separator: characterSet)
+        let reactions = scanner.parse()
+
+        let sut = SpaceStoichiometry()
+        XCTAssertEqual(sut.solvePartOne(reactions: reactions), 136771)
+    }
+}
+
+extension SpaceStoichiometry.Reaction: Scannable {
+    static func parse(scanner: Scanner) -> SpaceStoichiometry.Reaction? {
+        let idCharacterSet = CharacterSet.uppercaseLetters
+        var chemicals: [SpaceStoichiometry.Chemical] = []
+
+        var identifier: NSString?
+        var quantity: Int = 0
+
+        while scanner.scanInt(&quantity), scanner.scanCharacters(from: idCharacterSet, into: &identifier), let id = identifier as String? {
+            chemicals.append(SpaceStoichiometry.Chemical(id: id, quantity: quantity))
+        }
+        scanner.scanCharacters(from: .newlines, into: nil)
+
+        guard let result = chemicals.popLast() else {
+            return nil
+        }
+
+        return SpaceStoichiometry.Reaction(input: chemicals, output: result)
+    }
 }
