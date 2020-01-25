c8b9e10b650e14fc0f54506978d7cd7579289621
Xavier Jurado <xavier.jurado@gmail.com>
2020-01-25 02:59:45 +0100

    Day 16 Part 2

--- Puzzles/Puzzles/16-fft.swift
+++ Puzzles/Puzzles/16-fft.swift
@@ -11,16 +11,52 @@ class FlawedFrequencyTransmission {
     func solvePartOne(list: [Int], phases: Int = 1) -> [Int] {
         var input = list
         for _ in 0..<phases {
-            var output = Array(repeating: 0, count: input.count)
-            for i in 0..<input.count {
-                let pattern = computerPattern(at: i)
-                let patternLength = pattern.count
-                output[i] = input.enumerated().map { $0.element * pattern[$0.offset % patternLength] }.reduce(0, +) % 10
+            var output = input
+            var i = input.count - 1
+            while i >= 0 {
+                let slice = input[i+1..<input.count]
+                output[i] = input[i] + zip(slice, slice.indices).map { $0.0 * computerPattern(for: i, index: $0.1) }.reduce(0, +)
+                output[i] = output[i] % 10
                 output[i] = abs(output[i])
+                i -= 1
             }
-            input = output
+            swap(&input, &output)
         }
         return input
+    }
+
+    // hint: value(digit, phase) = value(digit + 1, phase) + value(digit, phase - 1)
+    func solvePartTwo(list: [Int]) -> [Int] {
+        let phases = 100
+        let offset = zip(0..<7, list.prefix(7).reversed()).reduce(0) { acc, tuple -> Int in acc + Int(pow(10, Double(tuple.0))) * tuple.1 }
+        var input = Array(Array(repeating: list, count: 10_000).flatMap { $0 }.dropFirst(offset))
+        var output = Array(repeating: 0, count: input.count)
+        for _ in 0..<phases {
+            var i = input.count - 1
+            var partialSum = 0
+            while i >= 0 {
+                output[i] = input[i] + partialSum
+                output[i] = output[i] % 10
+                partialSum = output[i]
+                i -= 1
+            }
+            swap(&input, &output)
+        }
+        return Array(input.prefix(8))
+    }
+
+    func computerPattern(for output: Int, index: Int) -> Int {
+        let correctedOutput = output + 1
+        let correctedIndex = (index + 1) % (correctedOutput * 4)
+        if correctedIndex < correctedOutput {
+            return 0
+        } else if correctedIndex < 2 * correctedOutput {
+            return 1
+        } else if correctedIndex < 3 * correctedOutput {
+            return 0
+        } else {
+            return -1
+        }
     }
 
     func computerPattern(at index: Int) -> [Int] {
--- Puzzles/PuzzlesTests/16-fft-test.swift
+++ Puzzles/PuzzlesTests/16-fft-test.swift
@@ -1,7 +1,15 @@
 import XCTest
+import OSLog
 @testable import Puzzles
 
 class FlawedFrequencyTransmissionTest: XCTestCase {
+
+    func testComputePattern() {
+        let sut = FlawedFrequencyTransmission()
+        let p1 = sut.computerPattern(at: 6)
+        let p2 = (0..<p1.count).map {sut.computerPattern(for: 6, index: $0) }
+        XCTAssertEqual(p1, p2)
+    }
 
     func testOnePhase() {
         let signal = [1,2,3,4,5,6,7,8]
@@ -15,7 +23,6 @@ class FlawedFrequencyTransmissionTest: XCTestCase {
         XCTAssertEqual(sut.solvePartOne(list: signal, phases: 4), [0,1,0,2,9,4,9,8])
     }
 
-
     func testPartOne() {
         guard let inputFile = Bundle.current.url(forResource: "16", withExtension: "txt"),
             let inputData = try? Data(contentsOf: inputFile),
@@ -27,5 +34,25 @@ class FlawedFrequencyTransmissionTest: XCTestCase {
         let sut = FlawedFrequencyTransmission()
         let output = sut.solvePartOne(list: signal, phases: 100)
         XCTAssertEqual(output.prefix(upTo: 8), [1, 1, 8, 3, 3, 1, 8, 8])
+    }
+
+    func testShorterPartTwo() {
+        let signal = "03036732577212944063491565474664".compactMap {  Int("\($0)") }
+        let sut = FlawedFrequencyTransmission()
+        let output = sut.solvePartTwo(list: signal)
+        XCTAssertEqual(output, [8,4,4,6,2,0,2,6])
+    }
+
+    func testPartTwo() {
+        guard let inputFile = Bundle.current.url(forResource: "16", withExtension: "txt"),
+            let inputData = try? Data(contentsOf: inputFile),
+            let inputString = String(data: inputData, encoding: .utf8) else {
+            fatalError("Could not read test case")
+        }
+
+        let signal = inputString.compactMap{ Int(String($0)) }
+        let sut = FlawedFrequencyTransmission()
+        let output = sut.solvePartTwo(list: signal)
+        XCTAssertEqual(output, [5,5,0,0,5,0,0,0])
     }
 }
