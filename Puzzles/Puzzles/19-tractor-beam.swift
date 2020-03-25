c1a323b97219fd3b56765935df4238d8af267d08
Xavier Jurado <xavier.jurado@gmail.com>
2020-03-25 22:40:19 +0100

    Day 19 Part 2

--- Puzzles/Puzzles/19-tractor-beam.swift
+++ Puzzles/Puzzles/19-tractor-beam.swift
@@ -26,7 +26,43 @@ class TractorBeamInterface: InputBuffer, OutputBuffer {
 
 class TractorBeam {
 
-    func solvePartOne(memory: [Int]) throws -> Int {
+    let memory: [Int]
+
+    init(memory: [Int]) {
+        self.memory = memory
+    }
+
+    struct XY: Hashable {
+        var x: Int
+        var y: Int
+
+        var left: XY {
+            return XY(x: x - 1, y: y)
+        }
+
+        var right: XY {
+            return XY(x: x + 1, y: y)
+        }
+
+        var up: XY {
+            return XY(x: x, y: y - 1)
+        }
+
+        var down: XY {
+            return XY(x: x, y: y + 1)
+        }
+
+        func down(times: Int) -> XY {
+            return XY(x: x, y: y + times)
+        }
+
+        func left(times: Int) -> XY {
+            return XY(x: x - times, y: y)
+        }
+
+    }
+
+    func solvePartOne() throws -> Int {
         var affectedPoints = 0
         let area = 50
         for y in 0..<area {
@@ -44,5 +80,38 @@ class TractorBeam {
             }
         }
         return affectedPoints
+    }
+
+    func solvePartTwo() throws -> Int {
+        let shipSize = 99
+        var topRightEdge = XY(x: 8, y: 9)
+        var topLeftEdge: XY?
+
+        while topLeftEdge == nil {
+            topRightEdge = topRightEdge.down
+            while isAffected(at: topRightEdge.right) {
+                topRightEdge = topRightEdge.right
+            }
+
+            let bottomLeftCandidate = topRightEdge.down(times: shipSize).left(times: shipSize)
+            guard bottomLeftCandidate.x >= 0 else { continue }
+            if isAffected(at: bottomLeftCandidate) {
+                precondition(isAffected(at: topRightEdge))
+                topLeftEdge = topRightEdge.left(times: shipSize)
+            }
+        }
+
+        return topLeftEdge!.x * 10_000 + topLeftEdge!.y
+    }
+
+    private func isAffected(at point: XY) -> Bool {
+        let computer = Intcode(memory: memory)
+        let interface = TractorBeamInterface()
+        computer.input = interface
+        computer.output = interface
+        interface.x = point.x
+        interface.y = point.y
+        try! computer.execute()
+        return interface.affected
     }
 }
--- Puzzles/PuzzlesTests/19-tractor-beam-test.swift
+++ Puzzles/PuzzlesTests/19-tractor-beam-test.swift
@@ -6,7 +6,14 @@ class TractorBeamTest: XCTestCase {
     func testSolvePartOne() throws {
         let scanner = SingleValueScanner<Int>(testCaseName: "19", separator: CharacterSet(charactersIn: ","))
         let program = scanner.parse()
-        let sut = TractorBeam()
-        XCTAssertEqual(try sut.solvePartOne(memory: program), 112)
+        let sut = TractorBeam(memory: program)
+        XCTAssertEqual(try sut.solvePartOne(), 112)
+    }
+
+    func testSolvePartTwo() throws {
+        let scanner = SingleValueScanner<Int>(testCaseName: "19", separator: CharacterSet(charactersIn: ","))
+        let program = scanner.parse()
+        let sut = TractorBeam(memory: program)
+        XCTAssertEqual(try sut.solvePartTwo(), 18261982)
     }
 }
