e5ffef57c6f299b414e01c29e9d11510730fabc6
Xavier Jurado <xavier.jurado@gmail.com>
2020-03-27 11:28:49 +0100

    Day 20 Part 2

--- Puzzles/Puzzles/20-donut-maze.swift
+++ Puzzles/Puzzles/20-donut-maze.swift
@@ -37,7 +37,6 @@ class DonutMaze {
         func right(times: Int) -> XY {
             return XY(x: x + times, y: y)
         }
-
     }
 
     struct Maze {
@@ -48,6 +47,17 @@ class DonutMaze {
         let outerBottomRight: XY
         let innerTopLeft: XY
         let innerBottomRight: XY
+        var portalByPosition: [XY: Portal] = [:]
+        var outerPortalByName: [String: Portal] = [:]
+        var innerPortalByName: [String: Portal] = [:]
+
+        var entrance: Portal {
+            outerPortalByName["AA"]!
+        }
+
+        var exit: Portal {
+            outerPortalByName["ZZ"]!
+        }
 
         init(raw: String) {
             matrix = raw.split(separator: "\n").map { Array($0.unicodeScalars) }
@@ -65,6 +75,91 @@ class DonutMaze {
             outerBottomRight = XY(x: width - leftMargin - 1, y: height - topMargin - 1)
             innerTopLeft = XY(x: innerLeftBorder, y: innerTopBorder)
             innerBottomRight = XY(x: width - innerLeftBorder - 1, y: height - innerTopBorder - 1)
+
+            // Horizontal outer portals
+            for x in outerTopLeft.x...outerBottomRight.x {
+                let topPosition = XY(x: x, y: outerTopLeft.y)
+                if self[topPosition] == "." {
+                    let c1 = self[topPosition.up.up]
+                    let c2 = self[topPosition.up]
+                    let portal = Portal(position: topPosition, name: "\(c1)\(c2)")
+                    portalByPosition[portal.position] = portal
+                    outerPortalByName[portal.name] = portal
+                }
+
+                let bottomPosition = XY(x: x, y: outerBottomRight.y)
+                if self[bottomPosition] == "." {
+                    let c1 = self[bottomPosition.down]
+                    let c2 = self[bottomPosition.down.down]
+                    let portal = Portal(position: bottomPosition, name: "\(c1)\(c2)")
+                    portalByPosition[portal.position] = portal
+                    outerPortalByName[portal.name] = portal
+                }
+            }
+
+            // Vertical outer portals
+            for y in outerTopLeft.y...outerBottomRight.y {
+                let leftPosition = XY(x: outerTopLeft.x, y: y)
+                if self[leftPosition] == "." {
+                    let c1 = self[leftPosition.left.left]
+                    let c2 = self[leftPosition.left]
+                    let portal = Portal(position: leftPosition, name: "\(c1)\(c2)")
+                    portalByPosition[portal.position] = portal
+                    outerPortalByName[portal.name] = portal
+                }
+
+                let rightPosition = XY(x: outerBottomRight.x, y: y)
+                if self[rightPosition] == "." {
+                    let c1 = self[rightPosition.right]
+                    let c2 = self[rightPosition.right.right]
+                    let portal = Portal(position: rightPosition, name: "\(c1)\(c2)")
+                    portalByPosition[portal.position] = portal
+                    outerPortalByName[portal.name] = portal
+                }
+            }
+
+            // Horizontal inner portals
+            for x in innerTopLeft.x...innerBottomRight.x {
+                let topPosition = XY(x: x, y: innerTopLeft.y)
+                if self[topPosition] == "." {
+                    let c1 = self[topPosition.down]
+                    let c2 = self[topPosition.down.down]
+                    let portal = Portal(position: topPosition, name: "\(c1)\(c2)")
+                    portalByPosition[portal.position] = portal
+                    innerPortalByName[portal.name] = portal
+                }
+
+                let bottomPosition = XY(x: x, y: innerBottomRight.y)
+                if self[bottomPosition] == "." {
+                    let c1 = self[bottomPosition.up.up]
+                    let c2 = self[bottomPosition.up]
+                    let portal = Portal(position: bottomPosition, name: "\(c1)\(c2)")
+                    portalByPosition[portal.position] = portal
+                    innerPortalByName[portal.name] = portal
+                }
+            }
+
+            // Vertical inner portals
+            for y in innerTopLeft.y...innerBottomRight.y {
+                let leftPosition = XY(x: innerTopLeft.x, y: y)
+                if self[leftPosition] == "." {
+                    let c1 = self[leftPosition.right]
+                    let c2 = self[leftPosition.right.right]
+                    let portal = Portal(position: leftPosition, name: "\(c1)\(c2)")
+                    portalByPosition[portal.position] = portal
+                    innerPortalByName[portal.name] = portal
+                }
+
+                let rightPosition = XY(x: innerBottomRight.x, y: y)
+                if self[rightPosition] == "." {
+                    let c1 = self[rightPosition.left.left]
+                    let c2 = self[rightPosition.left]
+                    let portal = Portal(position: rightPosition, name: "\(c1)\(c2)")
+                    portalByPosition[portal.position] = portal
+                    innerPortalByName[portal.name] = portal
+                }
+            }
+
         }
 
         subscript(pos: XY) -> UnicodeScalar {
@@ -81,103 +176,16 @@ class DonutMaze {
 
     func solvePartOne(input: String) -> Int {
         let maze = Maze(raw: input)
-        var portalByPosition: [XY: Portal] = [:]
-        var outerPortalByName: [String: Portal] = [:]
-        var innerPortalByName: [String: Portal] = [:]
-
-        // Horizontal outer portals
-        for x in maze.outerTopLeft.x...maze.outerBottomRight.x {
-            let topPosition = XY(x: x, y: maze.outerTopLeft.y)
-            if maze[topPosition] == "." {
-                let c1 = maze[topPosition.up.up]
-                let c2 = maze[topPosition.up]
-                let portal = Portal(position: topPosition, name: "\(c1)\(c2)")
-                portalByPosition[portal.position] = portal
-                outerPortalByName[portal.name] = portal
-            }
-
-            let bottomPosition = XY(x: x, y: maze.outerBottomRight.y)
-            if maze[bottomPosition] == "." {
-                let c1 = maze[bottomPosition.down]
-                let c2 = maze[bottomPosition.down.down]
-                let portal = Portal(position: bottomPosition, name: "\(c1)\(c2)")
-                portalByPosition[portal.position] = portal
-                outerPortalByName[portal.name] = portal
-            }
-        }
-
-        // Vertical outer portals
-        for y in maze.outerTopLeft.y...maze.outerBottomRight.y {
-            let leftPosition = XY(x: maze.outerTopLeft.x, y: y)
-            if maze[leftPosition] == "." {
-                let c1 = maze[leftPosition.left.left]
-                let c2 = maze[leftPosition.left]
-                let portal = Portal(position: leftPosition, name: "\(c1)\(c2)")
-                portalByPosition[portal.position] = portal
-                outerPortalByName[portal.name] = portal
-            }
-
-            let rightPosition = XY(x: maze.outerBottomRight.x, y: y)
-            if maze[rightPosition] == "." {
-                let c1 = maze[rightPosition.right]
-                let c2 = maze[rightPosition.right.right]
-                let portal = Portal(position: rightPosition, name: "\(c1)\(c2)")
-                portalByPosition[portal.position] = portal
-                outerPortalByName[portal.name] = portal
-            }
-        }
-
-        // Horizontal inner portals
-        for x in maze.innerTopLeft.x...maze.innerBottomRight.x {
-            let topPosition = XY(x: x, y: maze.innerTopLeft.y)
-            if maze[topPosition] == "." {
-                let c1 = maze[topPosition.down]
-                let c2 = maze[topPosition.down.down]
-                let portal = Portal(position: topPosition, name: "\(c1)\(c2)")
-                portalByPosition[portal.position] = portal
-                innerPortalByName[portal.name] = portal
-            }
-
-            let bottomPosition = XY(x: x, y: maze.innerBottomRight.y)
-            if maze[bottomPosition] == "." {
-                let c1 = maze[bottomPosition.up.up]
-                let c2 = maze[bottomPosition.up]
-                let portal = Portal(position: bottomPosition, name: "\(c1)\(c2)")
-                portalByPosition[portal.position] = portal
-                innerPortalByName[portal.name] = portal
-            }
-        }
-
-        // Vertical inner portals
-        for y in maze.innerTopLeft.y...maze.innerBottomRight.y {
-            let leftPosition = XY(x: maze.innerTopLeft.x, y: y)
-            if maze[leftPosition] == "." {
-                let c1 = maze[leftPosition.right]
-                let c2 = maze[leftPosition.right.right]
-                let portal = Portal(position: leftPosition, name: "\(c1)\(c2)")
-                portalByPosition[portal.position] = portal
-                innerPortalByName[portal.name] = portal
-            }
-
-            let rightPosition = XY(x: maze.innerBottomRight.x, y: y)
-            if maze[rightPosition] == "." {
-                let c1 = maze[rightPosition.left.left]
-                let c2 = maze[rightPosition.left]
-                let portal = Portal(position: rightPosition, name: "\(c1)\(c2)")
-                portalByPosition[portal.position] = portal
-                innerPortalByName[portal.name] = portal
-            }
-        }
 
         // BFS
-        let origin = outerPortalByName["AA"]!
+        let origin = maze.entrance
         var visited: [XY: Bool] = [origin.position: true]
         var queue: [XY] = [origin.position]
         var distTo: [XY: Int] = [origin.position : 0]
 
         while !queue.isEmpty {
             let v = queue.removeFirst()
-            if let portal = portalByPosition[v], portal.name == "ZZ" {
+            if let portal = maze.portalByPosition[v], portal == maze.exit {
                 return distTo[v]!
             }
 
@@ -189,18 +197,99 @@ class DonutMaze {
                 queue.append(n)
             }
 
-            if let portal = portalByPosition[v], neightbours.isEmpty {
-                if innerPortalByName[portal.name] == portal, let outerPortal = outerPortalByName[portal.name] {
+            if let portal = maze.portalByPosition[v], neightbours.isEmpty {
+                if maze.innerPortalByName[portal.name] == portal, let outerPortal = maze.outerPortalByName[portal.name] {
                     visited[outerPortal.position] = true
                     distTo[outerPortal.position] = distTo[v]! + 1
                     queue.append(outerPortal.position)
                 } else {
-                    let innerPortal = innerPortalByName[portal.name]!
+                    let innerPortal = maze.innerPortalByName[portal.name]!
                     visited[innerPortal.position] = true
                     distTo[innerPortal.position] = distTo[v]! + 1
                     queue.append(innerPortal.position)
                 }
 
+            }
+        }
+
+        fatalError()
+    }
+
+    struct XYZ: Hashable {
+        var x: Int
+        var y: Int
+        var z: Int
+
+        init(x: Int, y: Int, z: Int) {
+            self.x = x
+            self.y = y
+            self.z = z
+        }
+
+        init(_ p: XY, z: Int) {
+            x = p.x
+            y = p.y
+            self.z = z
+        }
+
+        var left: XYZ {
+            return XYZ(x: x - 1, y: y, z: z)
+        }
+
+        var right: XYZ {
+            return XYZ(x: x + 1, y: y, z: z)
+        }
+
+        var up: XYZ {
+            return XYZ(x: x, y: y - 1, z: z)
+        }
+
+        var down: XYZ {
+            return XYZ(x: x, y: y + 1, z: z)
+        }
+
+        var toXY: XY {
+            return XY(x: x, y: y)
+        }
+    }
+
+    func solvePartTwo(input: String) -> Int {
+        let maze = Maze(raw: input)
+
+        let origin = maze.entrance
+        let level0position = XYZ(origin.position, z: 0)
+        var visited: [XYZ: Bool] = [level0position: true]
+        var queue: [XYZ] = [level0position]
+        var distTo: [XYZ: Int] = [level0position : 0]
+
+        while !queue.isEmpty {
+            let v = queue.removeFirst()
+            if let portal = maze.portalByPosition[v.toXY], portal == maze.exit, v.z == 0 {
+                return distTo[v]!
+            }
+
+            let neightbours = [v.up, v.right, v.down, v.left].filter { maze[$0.toXY] == "." }.filter { visited[$0, default: false] == false }
+
+            for n in neightbours {
+                visited[n] = true
+                distTo[n] = distTo[v]! + 1
+                queue.append(n)
+            }
+
+            if let portal = maze.portalByPosition[v.toXY], neightbours.isEmpty, portal != maze.entrance, portal != maze.exit {
+                if maze.innerPortalByName[portal.name] == portal, let outerPortal = maze.outerPortalByName[portal.name] {
+                    let deeperLevelPosition = XYZ(outerPortal.position, z: v.z + 1)
+                    visited[deeperLevelPosition] = true
+                    distTo[deeperLevelPosition] = distTo[v]! + 1
+                    queue.append(deeperLevelPosition)
+                } else if v.z > 0 {
+                    // when at the outermost level, only the outer labels AA and ZZ function; all other outer labeled tiles are effectively walls
+                    let innerPortal = maze.innerPortalByName[portal.name]!
+                    let shallowerLevelPosition = XYZ(innerPortal.position, z: v.z - 1)
+                    visited[shallowerLevelPosition] = true
+                    distTo[shallowerLevelPosition] = distTo[v]! + 1
+                    queue.append(shallowerLevelPosition)
+                }
             }
         }
 
--- Puzzles/PuzzlesTests/20-donut-maze-test.swift
+++ Puzzles/PuzzlesTests/20-donut-maze-test.swift
@@ -25,4 +25,37 @@ class DonutMazeTest: XCTestCase {
         let sut = DonutMaze()
         XCTAssertEqual(sut.solvePartOne(input: maze), 644)
     }
+
+    func testRecursiveSample1() {
+        guard let inputFile = Bundle.current.url(forResource: "20-sample1", withExtension: "txt"),
+            let inputData = try? Data(contentsOf: inputFile),
+            let maze = String(data: inputData, encoding: .utf8) else {
+            fatalError("Could not read test case")
+        }
+
+        let sut = DonutMaze()
+        XCTAssertEqual(sut.solvePartTwo(input: maze), 26)
+    }
+
+    func testRecursiveSample2() {
+        guard let inputFile = Bundle.current.url(forResource: "20-sample2", withExtension: "txt"),
+            let inputData = try? Data(contentsOf: inputFile),
+            let maze = String(data: inputData, encoding: .utf8) else {
+            fatalError("Could not read test case")
+        }
+
+        let sut = DonutMaze()
+        XCTAssertEqual(sut.solvePartTwo(input: maze), 396)
+    }
+
+    func testPartTwo() {
+        guard let inputFile = Bundle.current.url(forResource: "20", withExtension: "txt"),
+            let inputData = try? Data(contentsOf: inputFile),
+            let maze = String(data: inputData, encoding: .utf8) else {
+            fatalError("Could not read test case")
+        }
+
+        let sut = DonutMaze()
+        XCTAssertEqual(sut.solvePartTwo(input: maze), 7798)
+    }
 }
